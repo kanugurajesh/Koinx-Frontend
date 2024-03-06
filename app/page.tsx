378fe4735625098d10166ce3923c3f8272e9f7df
kanugurajesh <kanugurajesh3@gmail.com>
2024-03-06 14:48:53 +0530

    Adding use effect request to fetch bitcion pricing data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,44 @@
+"use client"
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "@/styles/app.module.css";
+import toast, { Toaster } from "react-hot-toast";
 import Link from "next/link";
 
 export default function Home() {
+
+  const [bitcoinPrice, setBitcoinPrice] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        toast.loading("Fetching bitcoin data");
+        const response = await fetch(
+          "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr%2Cusd&include_24hr_change=true"
+        );
+
+        if (!response) {
+          toast.dismiss();
+          toast.error("Error fetching bitoin data");
+          return;
+        }
+
+        const data = await response.json();
+        toast.dismiss();
+        setBitcoinPrice(data);
+        toast.success("Bitcoin data fetched")
+      } catch (error) {
+        toast.dismiss()
+        toast.error("Cannot fetch bitcoin data");
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
+      <Toaster />
       <nav
         className={`flex justify-between items-center border-[#DEDFE2] border-bottom border-b-2 ${styles.navbar}`}
       >
@@ -31,7 +65,11 @@ export default function Home() {
           <li className="w-6 h-1 bg-black"></li>
         </ul>
       </nav>
-      <main className="bg-[#EFF2F5] h-full w-full">{/* data */}</main>
+      <main className={`bg-[#EFF2F5] h-full w-full ${styles.main}`}>
+        <p className="text-sm">
+          cryptocurrencies &gt;&gt; <span className="font-medium">Bitcoin</span>
+        </p>
+      </main>
     </div>
   );
 }
