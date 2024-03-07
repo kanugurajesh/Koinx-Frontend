8c40e52b009e93a80322c03a3d0da5e7ffc1121c
kanugurajesh <kanugurajesh3@gmail.com>
2024-03-07 18:30:07 +0530

    Adding performance card

--- app/page.tsx
+++ app/page.tsx
@@ -130,7 +130,7 @@ export default function Home() {
         <p className="text-sm mt-4 mb-2">
           Cryptocurrencies &gt;&gt; <span className="font-medium">Bitcoin</span>
         </p>
-        <section className={`${styles.section1} flex justify-around`}>
+        <section className={`${styles.section1} flex justify-between`}>
           <div className={`flex flex-col gap-2 ${styles.leftpanel}`}>
             <div className="flex flex-col p-6 bg-white rounded-md gap-6">
               <div className="flex flex-col gap-8">
@@ -267,6 +267,91 @@ export default function Home() {
                     </div>
                     );
                   })}
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className={`${styles.section2} bg-white rounded-md gap-8`}>
+          <div className="p-5 flex flex-col gap-8 w-[100%]">
+            <h1 className="font-semibold text-2xl">Performance</h1>
+            <div className="w-[100%] flex flex-col gap-10">
+              <div className="flex justify-between items-center w-[100%] gap-10">
+                <div className="flex flex-col gap-2 text-[#44475B]">
+                  <p className="text-[13.78px] text-nowrap">Today's Low</p>
+                  <p className="text=[#44475B] text-[15.63px] font-semibold">46,932.22</p>
+                </div>
+                <div className="w-[100%]">
+                  <div className={`bg-gradient-to-r from-[#FF4949] via-[#FF4E11] via-[#FC870A] via-[#FFAF11] via-[#C2CB21] to-[#11EB68] h-[10px] ${styles.colorline}`}></div>
+                </div>
+                <div className="flex flex-col gap-2 text-[#44475B]">
+                  <p className="text-[13.78px] text-nowrap">Today's High</p>
+                  <p className="text=[#44475B] text-[15.63px] font-semibold">49,343.83</p>
+                </div>
+              </div>
+              <div className="flex justify-between items-center w-[100%] gap-10">
+                <div className="flex flex-col gap-2 text-[#44475B]">
+                  <p className="text-[13.78px] text-nowrap">52W Low</p>
+                  <p className="text=[#44475B] text-[15.63px] font-semibold">16,930.22</p>
+                </div>
+                <div className="w-[100%]">
+                  <div className={`bg-gradient-to-r from-[#FF4949] via-[#FF4E11] via-[#FC870A] via-[#FFAF11] via-[#C2CB21] to-[#11EB68] h-[10px] ${styles.colorline}`}></div>
+                </div>
+                <div className="flex flex-col gap-2 text-[#44475B]">
+                  <p className="text-[13.78px] text-nowrap">52W High</p>
+                  <p className="text=[#44475B] text-[15.63px] font-semibold">49,743.83</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="p-5 flex flex-col gap-8 w-[100%]">
+            <div className="flex gap-2">
+              <h1 className="font-semibold text-lg text-[#44475B]">Fundamentals</h1>
+              <Image src="/question.svg" alt="question" width={20} height={20} />
+            </div>
+            <div className="flex justify-between">
+              <div className="w-[45%] flex flex-col gap-4">
+                <div className="flex justify-between border-b-2 border-[#DEE2E6] pb-5">
+                  <p className="text-[#768396] text-[14px]">Bitcoin Prize</p>
+                  <p className="font-medium text-[13px]">$16,815.46</p>
+                </div>
+                <div className="flex justify-between border-b-2 border-[#DEE2E6] pb-5">
+                  <p className="text-[#768396] text-[14px]">Bitcoin Prize</p>
+                  <p className="font-medium text-[13px]">$16,815.46</p>
+                </div>
+                <div className="flex justify-between border-b-2 border-[#DEE2E6] pb-5">
+                  <p className="text-[#768396] text-[14px]">Bitcoin Prize</p>
+                  <p className="font-medium text-[13px]">$16,815.46</p>
+                </div>
+                <div className="flex justify-between border-b-2 border-[#DEE2E6] pb-5">
+                  <p className="text-[#768396] text-[14px]">Bitcoin Prize</p>
+                  <p className="font-medium text-[13px]">$16,815.46</p>
+                </div>
+                <div className="flex justify-between border-b-2 border-[#DEE2E6] pb-5">
+                  <p className="text-[#768396] text-[14px]">Bitcoin Prize</p>
+                  <p className="font-medium text-[13px]">$16,815.46</p>
+                </div>
+              </div>
+              <div className="w-[45%] flex flex-col gap-4">
+                <div className="flex justify-between border-b-2 border-[#DEE2E6] pb-5">
+                  <p className="text-[#768396] text-[14px]">Bitcoin Prize</p>
+                  <p className="font-medium text-[13px]">$16,815.46</p>
+                </div>
+                <div className="flex justify-between border-b-2 border-[#DEE2E6] pb-5">
+                  <p className="text-[#768396] text-[14px]">Bitcoin Prize</p>
+                  <p className="font-medium text-[13px]">$16,815.46</p>
+                </div>
+                <div className="flex justify-between border-b-2 border-[#DEE2E6] pb-5">
+                  <p className="text-[#768396] text-[14px]">Bitcoin Prize</p>
+                  <p className="font-medium text-[13px]">$16,815.46</p>
+                </div>
+                <div className="flex justify-between border-b-2 border-[#DEE2E6] pb-5">
+                  <p className="text-[#768396] text-[14px]">Bitcoin Prize</p>
+                  <p className="font-medium text-[13px]">$16,815.46</p>
+                </div>
+                <div className="flex justify-between border-b-2 border-[#DEE2E6] pb-5">
+                  <p className="text-[#768396] text-[14px]">Bitcoin Prize</p>
+                  <p className="font-medium text-[13px]">$16,815.46</p>
+                </div>
               </div>
             </div>
           </div>
